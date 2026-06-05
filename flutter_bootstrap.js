1bbf98979b5292a39e63073e5ed07a36b6233643
Sidharth Lahoti <sidharthlahoti@Sidharths-MacBook-Pro.local>
2026-06-05 13:43:26 +0530

    Upload-only video submission

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4129036480" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1639334824" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
