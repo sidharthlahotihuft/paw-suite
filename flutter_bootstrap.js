7401226b95bb86d938969a104b5fb7c743e5539b
Sidharth Lahoti <sidharthlahoti@Sidharths-MacBook-Pro.local>
2026-06-05 12:46:47 +0530

    Replace camera with file picker on web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4222665073" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4259210589" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
