75ae41e7a1fab30b33899c2747a4c1a2fe914a2d
Sidharth Lahoti <sidharthlahoti@Sidharths-MacBook-Pro.local>
2026-06-05 13:34:35 +0530

    Simple video upload for web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4262449752" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4129036480" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
