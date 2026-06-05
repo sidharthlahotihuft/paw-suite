5b4ccb82e12496a45a78527fa35698a0a143adfe
Sidharth Lahoti <sidharthlahoti@Sidharths-MacBook-Pro.local>
2026-06-05 12:56:03 +0530

    File picker for web video upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1123497057" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4262449752" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
