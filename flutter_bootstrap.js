3b0384e831d2b77c92fdd9b1f318a21f1a1429f9
Sidharth Lahoti <sidharthlahoti@Sidharths-MacBook-Pro.local>
2026-06-08 22:36:37 +0530

    Replace scores with grades everywhere

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3559730827" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2919255212" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
