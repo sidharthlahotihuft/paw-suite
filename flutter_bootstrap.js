5e9954971e6aa56cb6f84c36664511def8bb5c87
Sidharth Lahoti <sidharthlahoti@Sidharths-MacBook-Pro.local>
2026-06-09 15:04:54 +0530

    feat: F1 snooze 48h, F2 call back date picker + not interested

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2988276171" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1381930804" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
