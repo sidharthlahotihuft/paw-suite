868e00fe153502ff641f42e4d7ae745d1a02544a
Sidharth Lahoti <sidharthlahoti@Sidharths-MacBook-Pro.local>
2026-06-09 14:41:32 +0530

    feat: remove Tomorrow and Overdue filters, keep All/Today/This Week/Calendar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2523928711" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3887453682" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
