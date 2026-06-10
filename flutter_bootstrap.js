2057fa6f58142fb22fd1dfc179b77e6bbdcc6098
Sidharth Lahoti <sidharthlahoti@Sidharths-MacBook-Pro.local>
2026-06-10 10:39:02 +0530

    feat: show submissions with scores on roleplay detail, 14-day auto-delete notice

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2842376739" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "534903259" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
