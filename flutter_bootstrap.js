bb158f6b220ddfcea5bf785c91ada73dbf4f8535
Sidharth Lahoti <sidharthlahoti@Sidharths-MacBook-Pro.local>
2026-06-11 12:29:11 +0530

    feat: show store employee submission count per theme, 10 employee limit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3159603415" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3437062270" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
