17c19bff444e96a5a2325aca543f65fc923dd09b
Sidharth Lahoti <sidharthlahoti@Sidharths-MacBook-Pro.local>
2026-06-11 13:59:01 +0530

    feat: re-record button for invalid submissions

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "661341800" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1797566620" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
